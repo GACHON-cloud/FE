630287d211c21fc194bec0bb97d52cf6f13e6b2e
연하영 <dusgkdud7@naver.com>
2023-10-20 05:07:55 +0900

    랜딩 페이지

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import * as React from 'react'
+import { Reset } from 'styled-reset'
 import './App.css';
+import Landing from './pages/Landing';
+import Onboarding from './pages/Onboarding';
+import {Routes, Route, Link} from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Reset />
+      <Routes>
+      <Route path="/" element={<Landing/>}/>
+      <Route path="/signup" element={<Onboarding/>}/>
+
+      </Routes>
     </div>
   );
 }
