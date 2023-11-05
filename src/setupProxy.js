90eae3de10d560b9d3979382f41db78dbd5d27a0
연하영 <dusgkdud7@naver.com>
2023-11-06 00:34:46 +0900

    프록시 설정

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://ceprj.gachon.ac.kr:60006', 
+    })
+  );
+};
