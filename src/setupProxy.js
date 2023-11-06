de88f1ec3500bff90786c50a031124994f9584e7
연하영 <dusgkdud7@naver.com>
2023-11-06 22:48:29 +0900

    setProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,11 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function(app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       target: 'http://ceprj.gachon.ac.kr:60006', 
-//     })
-//   );
-// };
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://ceprj.gachon.ac.kr:60006',	
+      changeOrigin: true,
+    })
+  );
+};
