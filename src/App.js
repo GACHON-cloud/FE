663dc609ad51c133c9e243b3cf7c39ad0d52b79a
연하영 <dusgkdud7@naver.com>
2023-11-14 22:40:55 +0900

    라이브러리 테스트

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Guide from './pages/Guide';
 import SignIn from './pages/SignIn';
 import RealityList from "./pages/RealityList"
 import Details from './pages/Details';
+import Carousel from 'react-material-ui-carousel';
 
 
 const serverURL ="http://ceprj.gachon.ac.kr:60006"
@@ -37,6 +38,7 @@ function App() {
         <Route path="/reality-list" element={<><Header/><RealityList/></>}/>
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='/details' element={<><Header/><Details/></>}/>
+        <Route path='/carousel' element={<><Header/><Carousel/></>}/>
       </Routes>
       
     </div>
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -7,6 +7,7 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Orders from '../components/Buildingdetail';
+import Carousel from '../components/Carousel';
 
 
 
@@ -39,7 +40,7 @@ export default function Details() {
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
            
-             
+             <Carousel/>
               {/* Recent Orders */}
               <Grid item xs={12}>
                 <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
