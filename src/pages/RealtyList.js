685e875ff4435c821b3a93ec6712c72fe3b0472e
연하영 <dusgkdud7@naver.com>
2023-11-15 22:52:49 +0900

    search api 삭제

--- src/pages/RealtyList.js
+++ src/pages/RealtyList.js
@@ -15,6 +15,8 @@ import Box from '@mui/material/Box';
 import { Pagination } from '@mui/material';
 import axios from 'axios';
 
+
+
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
@@ -53,25 +55,33 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
+
 export default function RealtyList() {
+
+  
+  const dividerStyle = {
+    margin: '0 0', // 양쪽 대칭
+    backgroundColor: 'rgba(0, 0, 0, 0.1)', 
+  };
+
+  
+  
   const [buildingList, setBuildingList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(20);
-  const [searchTerm, setSearchTerm] = useState('');
-
+  
   useEffect(() => {
-    if (searchTerm !== '') {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get(`http://ceprj.gachon.ac.kr:60014/building/search?q=${searchTerm}`);
-          setBuildingList(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      fetchData();
-    }
-  }, [searchTerm]);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://ceprj.gachon.ac.kr:60014/building/getAll');
+        setBuildingList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   // 현재 페이지에 해당하는 매물 목록을 가져오는 함수
   const getCurrentItems = () => {
@@ -85,83 +95,66 @@ export default function RealtyList() {
     setCurrentPage(page);
   };
 
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearchKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      // 검색어를 입력한 뒤 엔터를 누르면 API 호출
-      setSearchTerm(event.target.value);
-    }
-  };
-
   return (
-    <>
-      <Grid container direction="column" justifyContent="center" alignItems="center">
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar elevation={0} style={{ backgroundColor: 'transparent' }} position="static">
-            <Grid item style={{ margin: "100px 50px 50px 50px" }}>
-              <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  style={{ color: "#898989" }}
-                  placeholder="지역명 또는 단지명으로 검색해주세요."
-                  inputProps={{ 'aria-label': 'search' }}
-                  value={searchTerm}
-                  onChange={handleSearchChange}
-                  onKeyPress={handleSearchKeyPress}
-                />
-              </Search>
-            </Grid>
-          </AppBar>
-        </Box>
-        <List sx={{ textAlign: 'center', width: '70%', margin: '50px', bgcolor: 'background.paper' }}>
-          <Grid style={{ margin: '20px', textAlign: 'left', fontWeight: 'bold', color: '#414141' }}>검색 결과</Grid>
-          <Divider sx={{ margin: '0 0', backgroundColor: 'rgba(0, 0, 0, 0.1)' }} />
-
-          {getCurrentItems().map((building) => (
-            <React.Fragment key={building.id}>
-              <ListItem alignItems="center">
-                <ListItemAvatar>
-                  <img src="/images/testimg.png" width="200px" style={{ margin: '5px' }} alt="Building" />
-                </ListItemAvatar>
-                <div style={{ margin: '30px' }}>
-                  <ListItemText
-                    primary={<Typography variant="h5" style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>{building.address}</Typography>}
-                    secondary={
-                      <React.Fragment>
-                        <Typography
-                          sx={{ display: 'inline' }}
-                          component="span"
-                          variant="body2"
-                          color="text.primary"
-                          style={{ fontSize: '1.3em' }}
-                        >
-                          {building.dealPrice ? `매매 ${building.dealPrice}` : `전세 ${building.rentPrice}`}
-                        </Typography>
-                      </React.Fragment>
-                    }
-                  />
-                </div>
-              </ListItem>
-              <Divider sx={{ margin: '0 0', backgroundColor: 'rgba(0, 0, 0, 0.1)' }} />
-              <Divider variant="inset" component="li" />
-            </React.Fragment>
-          ))}
-
-          <Grid sx={{ justifyContent: 'center', marginTop: '20px' }}>
-            <Pagination
-              count={Math.ceil(buildingList.length / itemsPerPage)}
-              page={currentPage}
-              onChange={handlePageChange}
-            />
+       <>
+         <Grid container direction="column" justifyContent="center" alignItems="center">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar elevation={0} style={{ backgroundColor: 'transparent' }} position="static">
+          <Grid item style={{ margin: "100px 50px 50px 50px" }}>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase style={{ color: "#898989" }} placeholder="지역명 또는 단지명으로 검색해주세요." inputProps={{ 'aria-label': 'search' }} />
+            </Search>
           </Grid>
-        </List>
-      </Grid>
+        </AppBar>
+      </Box>
+      <List sx={{ textAlign: 'center', width: '70%', margin: '50px', bgcolor: 'background.paper' }}>
+        <Grid style={{ margin: '20px', textAlign: 'left', fontWeight: 'bold', color: '#414141' }}>검색 결과</Grid>
+        <Divider sx={dividerStyle} />
+
+        {getCurrentItems().map((building) => (
+          <React.Fragment key={building.id}>
+            <ListItem alignItems="center">
+              <ListItemAvatar>
+                <img src="/images/testimg.png" width="200px" style={{ margin: '5px' }} />
+              </ListItemAvatar>
+              <div style={{ margin: '30px' }}>
+                <ListItemText
+                  primary={<Typography variant="h5" style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>{building.buildingName}</Typography>}
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        sx={{ display: 'inline' }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                        style={{ fontSize: '1.3em' }}
+                      >
+                        {building.dealPrice ? `매매 ${building.dealPrice}` : `전세 ${building.rentPrice}`}
+                      </Typography>
+                    </React.Fragment>
+                  }
+                />
+              </div>
+            </ListItem>
+            <Divider sx={dividerStyle} />
+            <Divider variant="inset" component="li" />
+          </React.Fragment>
+        ))}
+
+        <Grid sx={{ justifyContent: 'center', marginTop: '20px' }}>
+          <Pagination
+            count={Math.ceil(buildingList.length / itemsPerPage)}
+            page={currentPage}
+            onChange={handlePageChange}
+          />
+        </Grid>
+
+      </List>
+    </Grid>
     </>
   );
 }
+ 이 코드에 적용해줘
