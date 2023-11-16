41cd2b15f2f6c4464cd7a64cb0c1878dc7476836
연하영 <dusgkdud7@naver.com>
2023-11-16 13:45:39 +0900

    이미지 경로 수정

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -16,7 +16,7 @@ function TestPage() {
       });
       const images = response.data;
       if (images && images.length > 0) {
-        setImage(`http://ceprj.gachon.ac.kr:60014/imgs/${buildingName}/${images[1]}`);
+        setImage(`https://palgongtea.s3.ap-northeast-2.amazonaws.com/imgs/${buildingName}/${images[1]}.jpg`);
       } else {
         setImage(null);
       }
